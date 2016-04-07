d53e755d803edfbd6d35130648384095093db724
James Gartrell <jgartrel@gmail.com>
2016-04-07 11:48:26 -0700

    Clean up after install of af_openssl test

--- test-profiles/local/af_openssl/install.sh
+++ test-profiles/local/af_openssl/install.sh
@@ -4,15 +4,16 @@ tar -zxvf openssl-1.0.1g.tar.gz
 
 cd openssl-1.0.1g/
 ./config no-zlib
-make
-echo \$? > ~/install-exit-status
+make && cp -p apps/openssl ~/openssl
+echo $? > ~/install-exit-status
 cd ~
+rm -rf openssl-1.0.1g.tar.gz openssl-1.0.1g
 
 (
 cat <<'EOF'
 #!/bin/sh
 
-./openssl-1.0.1g/apps/openssl $@ > $LOG_FILE 2>&1
+./openssl $@ > $LOG_FILE 2>&1
 echo $? > ~/test-exit-status
 
 EOF
