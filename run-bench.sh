39f2830ccb5cfde0e84204638b854a74e69f0dbe
James Gartrell <jgartrel@gmail.com>
2016-04-12 15:51:54 -0700

    Automatically upload test results to S3

--- run-bench.sh
+++ run-bench.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+##
+# Function to upload test-results to s3
+#
+upload_results () {
+  file=$(basename "$1")
+  bucket=af-pts-storage
+  resource="/${bucket}/test-results/${file}"
+  contentType="application/x-compressed-tar"
+  dateValue=`date +"%a, %d %b %Y %H:%M:%S %z"`
+  stringToSign="PUT\n\n${contentType}\n${dateValue}\n${resource}"
+  s3Key=AKIAJWUYPQLEXM3UHFDA
+  s3Secret=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
+  signature=`echo -en ${stringToSign} | openssl sha1 -hmac ${s3Secret} -binary | base64`
+  curl -X PUT -T "${file}" \
+    -H "Host: ${bucket}.s3.amazonaws.com" \
+    -H "Date: ${dateValue}" \
+    -H "Content-Type: ${contentType}" \
+    -H "Authorization: AWS ${s3Key}:${signature}" \
+        https://${bucket}.s3.amazonaws.com/test-results/${file}
+}
+
+
 # Create Base Variables
 SCRIPT_PATH=$(cd "$(dirname "$0")" && pwd)
 SCRIPT_NAME=$(basename "$0")
@@ -22,3 +44,6 @@ ${PTS} detailed-system-info > ${RUN_LOG} 2>&1
 ${PTS} batch-run local/af-system >> ${RUN_LOG} 2>&1
 cd ${RESULTS_DIR}/..
 tar -czf ${TEST_RESULTS_NAME}.tgz ${TEST_RESULTS_NAME}.log ${TEST_RESULTS_NAME}
+
+# Upload to S3
+upload_results ${TEST_RESULTS_NAME}.tgz
